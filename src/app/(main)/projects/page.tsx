84bedffa1a5310d1e65effabd84d56de217ba421
jose cardona <josesiprozmaster@gmail.com>
2024-10-18 08:56:02 -0500

    change enpoint deploy

--- src/app/(main)/projects/page.tsx
+++ src/app/(main)/projects/page.tsx
@@ -4,6 +4,5 @@ import { getProyectsService } from "@/services/projectService";
 
 export default async function ProjectView() {
   const projects: IResponseProject | null = await getProyectsService();
-  if (!projects) return null;
   return <SectionProjects projects={projects} />;
 }
